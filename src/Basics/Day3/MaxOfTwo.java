fedfb16d626b8b3523ce5ba00d50ad672ac05fee
Sujiii1803 <sujiii1803@gmail.com>
2025-12-16 14:34:34 +0530

    Added Day 3 Java basics - string handling and methods

---
+++ src/Basics/Day3/MaxOfTwo.java
@@ -0,0 +1,15 @@
+package Basics.Day3;
+
+public class MaxOfTwo {
+
+    static int max(int a, int b) {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Max = " + max(5, 9));
+    }
+}
---
+++ src/Basics/Day3/StringLength.java
@@ -0,0 +1,9 @@
+package Basics.Day3;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String text = "Java";
+
+        System.out.println("Length = " + text.length());
+    }
+}
