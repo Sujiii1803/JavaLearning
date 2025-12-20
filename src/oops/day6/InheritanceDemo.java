995b515c77e118e0d37b38f22f89ce22d483525c
Sujiii1803 <sujiii1803@gmail.com>
2025-12-20 17:20:40 +0530

    Added OOPS Day 6 - interface implementation examples

---
+++ src/oops/day6/InheritanceDemo.java
@@ -0,0 +1,21 @@
+package oops.day6;
+
+class Animal {
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
